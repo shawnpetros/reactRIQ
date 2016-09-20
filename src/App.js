7e1ddc7338ecd153d0d4270da5257c8b0924d971
Shawn Petros <arashcuzi@gmail.com>
2016-09-20 09:16:28 -0500

    moar housekeeping

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Header from './components/header';
 import ToolMain from './components/toolMain';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        	<ToolMain>
-		        <p className="App-intro">
-		          To get started, edit <code>src/App.js</code> and save to reload.
-		        </p>
-		      </ToolMain>
-      	<Footer />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Header />
+					<ToolMain>
+						<p className="App-intro">
+							To get started, edit <code>src/App.js</code> and save to reload.
+						</p>
+					</ToolMain>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
